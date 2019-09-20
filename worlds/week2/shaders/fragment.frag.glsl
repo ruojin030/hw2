2d7eafca5c6ca433eb456d5c2c9a0744d9ab6831
Ruojin Zhang <ruojin030@gmail.com>
2019-09-20 15:59:15 -0400

    snow man

--- worlds/week2/shaders/fragment.frag.glsl
+++ worlds/week2/shaders/fragment.frag.glsl
@@ -4,7 +4,7 @@ precision highp float; // HIGH PRECISION FLOATS
 uniform float uTime;   // TIME, IN SECONDS
 in vec3 vPos;          // POSITION IN IMAGE
 out vec4 fragColor;    // RESULT WILL GO HERE
-const int NS = 2; // Number of spheres in the scene
+const int NS = 3; // Number of spheres in the scene
 const int NL = 2; // Number of light sources in the scene
 const float fl = 40.;
 const vec3 E = vec3(0.,0.,1.); //10
@@ -25,7 +25,7 @@ float raySphere(vec3 v, vec3 w, vec4 s){
 }
 bool isInShadow(vec3 P, vec3 L, vec4 Sphere[NS]){ // is point P in shadow from light L?
     for(int i = 0;i<NS;i++){
-        if(raySphere(P,L,Sphere[i])>0.0001){
+        if(raySphere(P,L,Sphere[i])>0.001){
             return true;
         }//30
     }
@@ -33,10 +33,10 @@ bool isInShadow(vec3 P, vec3 L, vec4 Sphere[NS]){ // is point P in shadow from l
 }
 void main() {
 
-    Ldir[0] = normalize(vec3(20.,10.,10));
+    Ldir[0] = normalize(vec3(65,65,30));
     Lcol[0] = vec3(.5,.3,.5);
 
-    Ldir[1] = normalize(vec3(-5.,-5.,-5.));
+    Ldir[1] = normalize(vec3(-5.,-5.,-3.));
     Lcol[1] = vec3(.2,.3,0.5); //40
 
     Sphere[0]   = vec4(0.,0.0,0,0.4);
@@ -48,7 +48,12 @@ void main() {
     Ambient[1]  = vec3(.1,.1,0.1);
     Diffuse[1]  = vec3(.5,.5,0.5);
     Specular[1] = vec4(1.,1.,1.,20.); // 4th value is specular power //50
- 
+     
+    Sphere[2]   = vec4(.2,.55,3,.05);
+    Ambient[2]  = vec3(.1,0,0);
+    Diffuse[2]  = vec3(0.4,.0,.0);
+    Specular[2] = vec4(.6,.6,.6,10.); // 4th value is specular power
+
 
     vec3 v = vec3(0,0,fl);
     vec3 w = normalize(vec3(vPos.xy,-fl));
